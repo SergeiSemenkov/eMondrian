28aedb95681a8ac4446a6177fa32c44664bc686c
06791947f7d3 <semenkovsergei@mail.ru>
2021-11-08 19:43:29 +0300

    fix

---
+++ _nuxt/static/1636387430/docs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1636387430",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs",config:{_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
