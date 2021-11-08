41c48ba589a622b29964b61cfe57d93300cd0f99
06791947f7d3 <semenkovsergei@mail.ru>
2021-11-08 20:13:57 +0300

    child link fix

--- _nuxt/static/1636390816/docs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1636390816",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs",config:{_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
