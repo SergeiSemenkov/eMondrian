55d785e384e2345d8c8dc5f5db1f0b59d4158023
ssemenkoff <ssemenkoff@gmail.com>
2021-11-13 15:16:30 +0300

    Rebuild with analytics tag

--- _nuxt/static/1636805179/consulting/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1636805179",layout:"default",error:a,serverRendered:true,routePath:"\u002Fconsulting",config:{googleAnalytics:{id:void 0},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
