e67437dbc047c7e721fd095059fa604d4d96ef39
ssemenkoff <ssemenkoff@gmail.com>
2021-11-14 20:35:00 +0300

    Added github project source button

---
+++ _nuxt/static/1636911234/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1636911234",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{googleAnalytics:{id:"UA-212825139-1"},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
