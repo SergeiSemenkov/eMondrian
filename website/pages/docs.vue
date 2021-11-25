924619bbe887046974abc09f71783699e1fa1ebf
ssemenkoff <ssemenkoff@gmail.com>
2021-11-25 16:32:01 +0300

    Case fixed

--- website/pages/docs.vue
+++ website/pages/docs.vue
@@ -8,7 +8,7 @@ export default {
   layout: 'docs',
   mounted () {
     if (this.$route.fullPath === '/docs' || this.$route.fullPath === '/docs/') {
-      this.$router.push('/docs/introduction')
+      this.$router.push('/docs/Introduction')
     }
   }
 }
