0598d12b3e98f79d73d671da2f5ba47db901d444
ssemenkoff <ssemenkoff@gmail.com>
2021-11-25 16:27:39 +0300

    Changed middleware to mounted hook

--- _nuxt/static/1637846557/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1637846557",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{googleAnalytics:{id:"UA-212825139-1"},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
