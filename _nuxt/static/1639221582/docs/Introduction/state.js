2bdb501fc807476b75048a018a7ccdc20179910c
06791947f7d3 <semenkovsergei@mail.ru>
2021-12-11 14:54:51 +0300

    tableau connect

--- _nuxt/static/1639221582/docs/Introduction/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1639221582",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fdocs\u002FIntroduction",config:{googleAnalytics:{id:"UA-212825139-1"},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1639223569/docs/client_tools/tableau_connect/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1639223569",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fclient_tools\u002Ftableau_connect",config:{googleAnalytics:{id:"UA-212825139-1"},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
