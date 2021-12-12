1a3e105b1585ed814fdb7466bf039f31681e67de
06791947f7d3 <semenkovsergei@mail.ru>
2021-12-12 15:00:54 +0300

    install on windows

--- _nuxt/static/1639309465/docs/client_tools/excel_connect/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FeMondrian\u002F_nuxt\u002Fstatic\u002F1639309465",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fclient_tools\u002Fexcel_connect",config:{googleAnalytics:{id:"UA-212825139-1"},_app:{basePath:"\u002FeMondrian\u002F",assetsPath:"\u002FeMondrian\u002F_nuxt\u002F",cdnURL:a}}}}(null));
